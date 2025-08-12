c563a934deb1642a0e6daccf674769d3d122522a
EukeGD <gayspastic98@gmail.com>
2025-08-12 20:39:48 +0000

    create preprocessor

---
+++ preprocessor.py
@@ -0,0 +1,14 @@
+## INFILE-LICENSE: GNU GPLv3
+# -*- geoscript-compiler -*- ---------------------
+#
+#   Copywrite (C) EukeGD, All rights reserved
+#
+#   See LICENSE or README for more infomation
+#
+# ------------------------------------------------
+#
+#   file: preprocessor.py
+#
+#   Script for handling preprocessor and data keywords
+#
+# ------------------------------------------------
