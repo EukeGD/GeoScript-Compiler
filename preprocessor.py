b1bfb004539e3a672c027638af9b7e68b854726f
EukeGD <gayspastic98@gmail.com>
2025-08-13 22:03:22 +0000

    wrote preprocessor module

--- preprocessor.py
+++ preprocessor.py
@@ -12,3 +12,132 @@
 #   Script for handling preprocessor and data keywords
 #
 # ------------------------------------------------
+
+import gddata as gdata
+from components import error_handler
+import json5
+
+class InvalidDataTagKeyword(error_handler.ErrorType): ...
+class InvalidPreprocessorKeyword(error_handler.ErrorType): ...
+
+class ParseForSingleFile:
+    def __init__(self, file: str, string: str) -> None:
+        self.file = file
+        self.gddata = gdata.readJSONData()
+        self.filelist = string.splitlines()
+            
+        self.datatags = {
+            'ips': 30,
+            'maxgroups': 9999,
+            'maxver': '2.207',
+            'minver': '2.200',
+            'targetver': '2.207'
+        }
+        self.preprocessor_data: dict[dict, list, dict[list, list]] = {
+            'macro': {},
+            'controll-flow': {
+                'start': [],
+                'end': []
+            }
+        }
+            
+        for ln, line in enumerate(self.filelist):
+            self.check_data_tags(line, ln)
+            self.preprocessor_keywords(line, ln)
+            
+    def check_data_tags(self, line: str, ln: int) -> None:
+        if line.startswith('@') == True:
+            if line.startswith('@ips:') == True:
+                line = line.removeprefix('@ips:')
+                line = line.removeprefix(' ')
+                self.datatags['ips'] = int(line)
+            elif line.startswith('@maxgroups:') == True:
+                line = line.removeprefix('@maxgroups:')
+                line = line.removeprefix(' ')
+                self.datatags['maxgroups'] = int(line)
+            elif line.startswith('@maxver:') == True:
+                line = line.removeprefix('@maxver:')
+                line = line.removeprefix(' ')
+                self.datatags['maxver'] = line
+            elif line.startswith('@minver:') == True:
+                line = line.removeprefix('@minver:')
+                line = line.removeprefix(' ')
+                self.datatags['minver'] = line
+            elif line.startswith('@targetver:') == True:
+                line = line.removeprefix('@targetver:')
+                line = line.removeprefix(' ')
+                self.datatags['targetver'] = line
+                
+                if self.gddata['gd-data']['version'] == line:
+                    print(f'{self.file}, line {ln}')
+                    print(f'  {line}\n')
+                    print(f'VersionWarning: Script target version {line} does not match the version listed in gddata.json {self.gddata['gd-data']['version']} | This could cause issues or crashes upon opening the level')
+            else:
+                error_handler.HandleErrorCase(InvalidDataTagKeyword, f'Data tag "{line}" doesnt exist', self.file, ln, line, [0, len(line)]).throw_with_traceback()
+    
+    def preprocessor_keywords(self, line: str, ln: int) -> None:
+        if line.startswith('#') == True:
+            if line.startswith('#define ') == True:
+                line = line.removeprefix('#define ')
+                if ' ' in line:
+                    line = line.split(' ')
+                    self.preprocessor_data['macro'][line[0]] = line[1]
+                else:
+                    self.preprocessor_data['macro'][line[0]] = None
+            elif line.startswith('#ifdef ') == True:
+                line = line.removeprefix('#ifdef ')
+                self.preprocessor_data['controll-flow']['start'].append({'start': ln, 'macro': line, 'expected': '§GSDEFINED'})
+            elif line.startswith('#ifndef ') == True:
+                line = line.removeprefix('#ifndef ')
+                self.preprocessor_data['controll-flow']['start'].append({'start': ln, 'macro': line, 'expected': '§GSDEFINED'})
+            elif line.startswith('#if ') == True:
+                line = line.removeprefix('#if ')
+                line = line.split(' ')
+                self.preprocessor_data['controll-flow']['start'].append({'start': ln, 'macro': line[0], 'expected': line[1]})
+            elif line.startswith('#endif') == True:
+                self.preprocessor_data['controll-flow']['end'].append(ln)
+
+
+
+def main(file: str) -> None:
+    with open(file) as rstream:
+        gddata = gdata.readJSONData()
+        usedfiles = [file]
+        stringstream = rstream.read()
+            
+        while True:
+            for line in stringstream.splitlines():
+                if line.startswith('#include ') == True:
+                    line = line.removeprefix('#include ')
+                    
+                    if line.startswith('<') == True:
+                        line = line.removeprefix('<')
+                        line = line.removesuffix('>')
+                        
+                        if not f'{gddata['geoscript']['stdlib-dir']}/{line}' in usedfiles:
+                            with open(f'{gddata['geoscript']['stdlib-dir']}/{line}') as libstream:
+                                usedfiles.append(f'{gddata['geoscript']['stdlib-dir']}/{line}')
+                                stringstream.replace(f'#include <{line}>', libstream.read())
+                    if line.startswith('"') == True:
+                        line = line.removeprefix('"')
+                        line = line.removesuffix('"')
+                        
+                        if not f'{gddata['geoscript']['stdlib-dir']}/{line}' in usedfiles:
+                            with open(f'./{line}') as libstream:
+                                usedfiles.append(f'./{line}')
+                                stringstream.replace(f'#include "{line}"', libstream.read())
+            break
+        
+        parsedfile = ParseForSingleFile(file, stringstream)
+        for macro in parsedfile.preprocessor_data['macros']:
+            if not parsedfile.preprocessor_data['macros'][macro] == None:
+                stringstream.replace(macro, parsedfile.preprocessor_data['macros'][macro])
+        
+        
+        with open('datatags.json', 'w') as wstrm:
+            json5.dump(parsedfile.datatags, wstrm)
+        
+        with open('preprocessed.gsi', 'w') as wstrm:
+            wstrm.write(stringstream)
+        
+        exit(0)
